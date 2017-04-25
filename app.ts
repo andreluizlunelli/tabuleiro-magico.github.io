2aa585b90d3f7cf776415ea2fac7f33f49bca841
André Lunelli <to.lunelli@gmail.com>
2017-04-25 19:41:27 -0300

    alterado construtor passando o contexto, agora da pra chamar as minhas funções dentro de update()

--- app.ts
+++ app.ts
@@ -1,95 +1,82 @@
-﻿
+﻿/// <reference path="typescript/phaser.d.ts" />
+
 class SimpleGame {
 
     constructor(width, height) {
         this.w = width;
         this.h = height;
-        this.game = new Phaser.Game(width, height, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update });
+        //this.game = new Phaser.Game(width, height, Phaser.AUTO, "content", { preload: this.preload, create: this.create, update: this.update, render: this.render });
+        this.game = new Phaser.Game(width, height, Phaser.AUTO, "content", this);
     }
 
     w: number;
     h: number;
     game: Phaser.Game;
-    platforms: Phaser.Group;
-    player: Phaser.Sprite;
     cursors: Phaser.CursorKeys;
 
+    box: Phaser.Sprite;
+    tween: Phaser.Tween;
+    counter: number;
+    step: number;
+
     preload() {
-        this.game.load.image('sky', 'assets/sky.png');
-        this.game.load.image('ground', 'assets/platform.png');
-        this.game.load.image('star', 'assets/star.png');
-        this.game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+        this.game.load.image("box", "assets/box.png");
     }
 
     create() {
         this.game.stage.backgroundColor = "#51E898";
         //  We're going to be using physics, so enable the Arcade Physics system
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-               
-        //  The platforms group contains the ground and the 2 ledges we can jump on
-        this.platforms = this.game.add.group();
-        //  We will enable physics for any object that is created in this group
-        this.platforms.enableBody = true;
-        // Here we create the ground.
-        //var ground = this.platforms.create(0, this.game.world.height - 64, 'ground');
-        var ground = this.platforms.create(this.w, this.game.world.height - 64, 'ground');
-        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-        ground.scale.setTo(2, 2);
-        //  This stops it from falling away when you jump on it
-        ground.body.immovable = true;
-        //  Now let's create two ledges
-        var ledge = this.platforms.create(400, 400, 'ground');
-        ledge.body.immovable = true;
-        ledge = this.platforms.create(-150, 250, 'ground');
-        ledge.body.immovable = true;
 
-        // [PLAYER]
+        // tamanho box 256x256
+        var w = (this.game.world.width / 2) - 128; // 128
+        var h = (this.game.world.height / 2) - 128; // 128
 
-        // The player and its settings
-        this.player = this.game.add.sprite(32, this.game.world.height - 150, 'dude');
-        //  We need to enable physics on the player
-        this.game.physics.arcade.enable(this.player);
-        //  Player physics properties. Give the little guy a slight bounce.
-        this.player.body.bounce.y = 0.2;
-        this.player.body.gravity.y = 300;
-        this.player.body.collideWorldBounds = true;
-        //  Our two animations, walking left and right.
-        this.player.animations.add('left', [0, 1, 2, 3], 10, true);
-        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-        this.player.body.gravity.y = 300;
-
+        this.box = this.game.add.sprite(w, h, 'box');
         this.cursors = this.game.input.keyboard.createCursorKeys();
+        
+        this.counter = 0;
+        this.step = Math.PI * 2 / 360;
+       
+
+
+        //this.game.add.tween(this.box).to( { x:this.box.x+20 }, 200, null, true, 0).to( { x:this.box.x }, 200, null, true, 3, 3);      
+        //this.shakeBox(this);
+        
     }
 
-    update() {
-        var hitPlatform = this.game.physics.arcade.collide(this.player, this.platforms);
-        //  Reset the players velocity (movement)
-        this.player.body.velocity.x = 0;
+    update() {        
 
-        if (this.cursors.left.isDown) {
-            //  Move to the left
-            this.player.body.velocity.x = -150;
+        /*
+        var tStep = Math.sin( this.counter ) ;
+        this.box.y = ((this.game.world.height / 2) - 128) + tStep * 30 ;
+        this.box.x = ((this.game.world.width / 2) - 128) + tStep * 30 ;
+        this.box.z = this.game.world.width + tStep * 30 ;
+        this.box.rotation += Phaser.Math.degToRad( 0.1 * tStep ) ;               
+        this.counter += this.step ;
+        */
+        if (this.cursors.down.isDown) {                        
+            //this.game.stage.backgroundColor = "#000000";    
+            this.minhafuncao();
 
-            this.player.animations.play('left');
+            //  And this tells it to yoyo, i.e. fade back to zero again before repeating.
+            //  The 3000 tells it to wait for 3 seconds before starting the fade back.
+            //this.tween.yoyo(true, 3000);
+
         }
-        else if (this.cursors.right.isDown) {
-            //  Move to the right
-            this.player.body.velocity.x = 150;
+    }  
 
-            this.player.animations.play('right');
-        }
-        else {
-            //  Stand still
-            this.player.animations.stop();
+    render() {        
+        this.game.debug.cameraInfo(this.game.camera, 32, 32);        
+    }
 
-            this.player.frame = 4;
-        }
+    minhafuncao() {
+        console.log('entrou minhafuncao');
+    }
 
-        //  Allow the player to jump if they are touching the ground.
-        if (this.cursors.up.isDown && this.player.body.touching.down && hitPlatform) {
-            this.player.body.velocity.y = -350;
-        }
+    shakeBox(that) {
+        var _x = that.box.x;
+        that.game.add.tween(that.box).to( { x:that.box.x+20 }, 200, null, true, 0).to( { x:_x }, 200, null, true, 3, 3);      
     }
 
 }
@@ -101,3 +88,10 @@ window.onload = () => {
     var game = new SimpleGame(w, h);
 
 };
+
+function wiggle(aProgress: number, aPeriod1: number, aPeriod2: number): number {
+            var current1: number = aProgress * Math.PI * 2 * aPeriod1;
+            var current2: number = aProgress * (Math.PI * 2 * aPeriod2 + Math.PI / 2);
+
+            return Math.sin(current1) * Math.cos(current2);
+        }
