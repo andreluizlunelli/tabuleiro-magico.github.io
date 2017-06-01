d01ff863582dd5c3a1fa3fa35d59414ee809e226
João Luiz Fernandes <Joaooluizzf@gmail.com>
2017-05-31 21:08:22 -0300

    apresentação

--- app.ts
+++ app.ts
@@ -1,124 +1,87 @@
 /// <reference path="typescript/phaser.d.ts" />
+// <reference path="node_modules/phaser-input/build/phaser-input.d.ts" />
 
 class SimpleGame {
 
-    constructor(width: number, height: number, mirror: boolean) {
+    constructor(width: number, height: number) {
         this.w = width;
-        this.h = height;        
-        this.game = new Phaser.Game(width, height, Phaser.AUTO, "content", this);        
-        this.espelhar = mirror;
+        this.h = height;
+        this.game = new Phaser.Game(width, height, Phaser.AUTO, "content", this);
+        //this.game.plugins.add(Fabrique.Plugins.InputField);
     }
 
-    w: number; // total tela
-    h: number; // total tela
+    w: number;
+    h: number;
     game: Phaser.Game;
     cursors: Phaser.CursorKeys;
 
     box: Phaser.Sprite;
+
+    upa: Phaser.Sprite;
+    downa: Phaser.Sprite;
+
     tween: Phaser.Tween;
     counter: number;
     step: number;
-    espelhar: boolean;
-
-    centena: Array<Phaser.Sprite> = [];
-    dezena: Array<Phaser.Sprite> = [];
-    unidade: Array<Phaser.Sprite> = [];
 
     preload() {
-        this.game.load.image("box", "assets/background.png");
-        this.game.load.image("circle_red", "assets/circle_red.svg");
-        this.game.load.image("circle_yellow", "assets/circle_yellow.svg");
-        this.game.load.image("circle_blue", "assets/circle_blue.svg");
+        this.game.load.image("background", "assets/background.png");
+        this.game.load.image("upa", "assets/background.png");
+        this.game.load.image("downa", "assets/background.png");
     }
 
-    create() {        
-
-        this.game.stage.backgroundColor = "#51E898";        
-
+    create() {
+        this.game.stage.backgroundColor = "#0b5546";
         //  We're going to be using physics, so enable the Arcade Physics system
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        // tamanho box 256x256
-        var w = (this.game.world.width / 2) - 128; // 128
-        var h = (this.game.world.height / 2) - 128; // 128
+        // tamanho background 500x700
+        var w = (this.game.world.width / 2) - 350; // 128
+        var h = (this.game.world.height / 2) - 225; // 128
 
-        this.box = this.game.add.sprite(w, h, 'box');
+        this.box = this.game.add.sprite(w, h, 'background');
         this.box.inputEnabled = true;
-        this.box.events.onInputDown.add(function() {            
-            this.fadeBox();
+        this.box.events.onInputDown.add(function() {
+            this.shakeBox();
+        }, this);
+
+        
+        // var text = game.add.bitmapText(400, 300, 'desyrel', 'Middle Earth', 64);       
+        this.upa = this.game.add.sprite(w, h, 'upa');
+        this.upa.inputEnabled = true;
+        this.upa.events.onInputDown.add(function() {
+            //TODO
+        }, this);
+
+        this.downa = this.game.add.sprite(w, h, 'downa');
+        this.downa.inputEnabled = true;
+        this.downa.events.onInputDown.add(function() {
+            //TODO
         }, this);
 
         this.cursors = this.game.input.keyboard.createCursorKeys();
-        
+
         this.counter = 0;
         this.step = Math.PI * 2 / 360;
-       
-        this.game.input.addPointer();
-        this.game.input.addPointer();
-        this.game.input.addPointer();
-        this.game.input.addPointer();
-        this.game.input.addPointer();
 
-        var a = this.getRandomSetParaImprimirNaTela(2);
-        var centena = a[0][0];
-        var dezena = a[0][1];
-        var unidade = a[0][2];
-        this.criarBolinhasNaTela(centena, dezena, unidade);
 
-        if (this.espelhar) { // espelhar
-            // ISSO AQUI FUNCIONA
-            // var g = this.game.add.group();
-            // g.x = 500;
-            //
-            // this.d = g.create(100, 300, 'content');
-            // this.d.anchor.setTo(0, 0);
-            //
-            // this.d.angle = 0;
-            // this.game.world.setBounds(100, 100, 2000, 2000);
-        }
+
     }
 
     update() {
-
-        /*
-        var tStep = Math.sin( this.counter ) ;
-        this.box.y = ((this.game.world.height / 2) - 128) + tStep * 30 ;
-        this.box.x = ((this.game.world.width / 2) - 128) + tStep * 30 ;
-        this.box.z = this.game.world.width + tStep * 30 ;
-        this.box.rotation += Phaser.Math.degToRad( 0.1 * tStep ) ;
-        this.counter += this.step ;
-        */
+        //todo fazer o update do texto baseado em uma variavel.
+    //text.setText("- You have clicked -\n" + count + " times !"); <- vai pro metodo upload, alterar com o valor que o usuario seleciona pelas cetas
+        
         if (this.cursors.down.isDown) {
-            //this.game.stage.backgroundColor = "#000000";
             this.shakeBox();
-
-            //  And this tells it to yoyo, i.e. fade back to zero again before repeating.
-            //  The 3000 tells it to wait for 3 seconds before starting the fade back.
-            //this.tween.yoyo(true, 3000);
         }
     }
 
     render() {
-        this.game.debug.cameraInfo(this.game.camera, 32, 32);
-
-        this.game.debug.pointer(this.game.input.mousePointer);
-        this.game.debug.pointer(this.game.input.pointer1);
-        this.game.debug.pointer(this.game.input.pointer2);
-        this.game.debug.pointer(this.game.input.pointer3);
-        this.game.debug.pointer(this.game.input.pointer4);
-        this.game.debug.pointer(this.game.input.pointer5);
-        this.game.debug.pointer(this.game.input.pointer6);
-        this.game.debug.pointer(this.game.input.pointer7);
-        this.game.debug.pointer(this.game.input.pointer8);
-    }
-
-    fadeBox() {
-        console.log('fade');
-        this.game.add.tween(this.box).to( { alpha: 0 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, true);
+                
     }
 
     shakeBox() {
-        console.log('shakeBox');
         this.game.add.tween(this.box)
         .to( { x:this.box.x+10 }, 100, null, true, 0)
         .to( { x:this.box.x-10 }, 100, null, true, 0)
@@ -126,109 +89,17 @@ class SimpleGame {
         .to( { x:this.box.x-10 }, 100, null, true, 0);
     }
 
-    getRandomSetParaImprimirNaTela(qtdRodadas: number) {
-        var listaRetornaRandomicos = [];
-        var inteirosJaEscolhidos = [];
-        var a = [
-            [9, 9, 9]
-            , [1, 2, 3]
-            , [4, 5, 6]
-            , [6, 2, 0]
-            , [0, 5, 5]
-            , [0, 2, 1]            
-            , [0, 1, 8]            
-            , [0, 1, 6]            
-            , [0, 0, 6]            
-            , [0, 3, 1]            
-        ];
-        if (qtdRodadas > a.length) {
-            alert("Numero de rodadas maior que o permitido");
-        }
-        for (var i = 0; i < qtdRodadas; i++) {
-            var randomint = this.getRandomInt(0, qtdRodadas);
-            // verifico se o numero já existe na lista dos escolhidos
-            for (var j = 0; j < inteirosJaEscolhidos.length; j++) {
-                if (inteirosJaEscolhidos[j] === randomint) {
-                    randomint = this.getRandomInt(0, qtdRodadas);
-                }
-            }
-            inteirosJaEscolhidos.push(randomint);
-            var item = a[randomint];
-            listaRetornaRandomicos.push(item);
-        }
-        return listaRetornaRandomicos;
-    }
-
-    /**
-     * Returns a random integer between min (inclusive) and max (inclusive)
-     * Using Math.round() will give you a non-uniform distribution!
-     */
-    getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    criarBolinhasNaTela(centena:number, dezena:number, unidade:number) {
-
-        var centralizarNoCubo = 90;
-        var fixedY = ((this.game.world.height / 2) - centralizarNoCubo) + 10;
-        var colunaBolinhasX = (this.game.world.width / 2) - centralizarNoCubo;
-        
-        var espacoColunas = 75;
-
-        var w, h, x = 0;
-        var y = fixedY;        
-        let b;
-
-        // centena
-        for (var i = 0; i < centena; ++i) {
-            x = (i % 2 == 0) ? colunaBolinhasX : colunaBolinhasX+15;
-            y += 15;
-
-            b = this.game.add.sprite(x, y, 'circle_blue');
-            b.width = 15;
-            b.height = 15;
-            this.centena.push( b );
-        }
-
-        y = fixedY;
-        colunaBolinhasX += espacoColunas;
-        // dezena
-        for (var i = 0; i < dezena; ++i) {
-            x = (i % 2 == 0) ? colunaBolinhasX : colunaBolinhasX+15;
-            y += 15;
-
-            b = this.game.add.sprite(x, y, 'circle_yellow');
-            b.width = 15;
-            b.height = 15;
-            this.centena.push( b );
-        }
-
-        y = fixedY;
-        colunaBolinhasX += espacoColunas;
-        // unidade
-        for (var i = 0; i < unidade; ++i) {
-            x = (i % 2 == 0) ? colunaBolinhasX : colunaBolinhasX+15;
-            y += 15;
-
-            b = this.game.add.sprite(x, y, 'circle_red');
-            b.width = 15;
-            b.height = 15;
-            this.centena.push( b );
-        }
-
-        this.game.world.bringToTop(this.box);
-    }
 }
 
 class ManagerGames {
-    
+
     constructor(numInstances: number, w: number, h: number) {
         if (numInstances < 1 || numInstances > 4) {
             alert("Numero de instâncias simultâneas é de 1 à 4");
         }
         this.numInstances = numInstances;
         this.w = w;
-        this.h = h;        
+        this.h = h;
     }
 
     numInstances: number;
@@ -239,61 +110,47 @@ class ManagerGames {
     start() {
         switch (this.numInstances) {
             case 1:
-                this.list.push(new SimpleGame(this.w, this.h, false));              
+                this.list.push(new SimpleGame(this.w, this.h));
                 break;
-            
+
             case 2:
-                this.list.push(new SimpleGame(this.w/2, this.h, false));
-                this.list.push(new SimpleGame(this.w/2, this.h, false));
+                this.list.push(new SimpleGame(this.w/2, this.h));
+                this.list.push(new SimpleGame(this.w/2, this.h));
                 break;
-            
+
             case 3:
-                this.list.push(new SimpleGame(this.w/2, this.h/2, true));
-                this.list.push(new SimpleGame(this.w/2, this.h/2, true));
-                this.list.push(new SimpleGame(this.w/2, this.h/2, false));
+                this.list.push(new SimpleGame(this.w/2, this.h/2));
+                this.list.push(new SimpleGame(this.w/2, this.h/2));
+                this.list.push(new SimpleGame(this.w/2, this.h/2));
                 break;
-            
-            case 4:                
-                this.list.push(new SimpleGame(this.w/2, this.h/2, true));
-                this.list.push(new SimpleGame(this.w/2, this.h/2, true));
-                this.list.push(new SimpleGame(this.w/2, this.h/2, false));
-                this.list.push(new SimpleGame(this.w/2, this.h/2, false));
+
+            case 4:
+                this.list.push(new SimpleGame(this.w/2, this.h/2));
+                this.list.push(new SimpleGame(this.w/2, this.h/2));
+                this.list.push(new SimpleGame(this.w/2, this.h/2));
+                this.list.push(new SimpleGame(this.w/2, this.h/2));
                 break;
-            
+
             default:
                 alert("valor inválido");
                 break;
         }
-        
+
     }
 }
 
-declare var mg: ManagerGames;
 window.onload = () => {
-
-    var p = getParameterByName('screen');
-    var _int = +p;
-    if (!_int) {
-        _int = 1;
-    }
-
-    // var i = window.prompt("Digite a quantidade de instâncias do jogo(1 à 4)", "");
-    // var _int = +i;
-
-    // var _int = 1;
+    var i = window.prompt("Digite a quantidade de instâncias do jogo(1 à 4)", "");
+    var _int = +i;
     var w = window.innerWidth;
     var h = window.innerHeight;
-    mg = new ManagerGames(_int, w, h);
-    mg.start();
+    var mg= new ManagerGames(_int, w, h);
+        mg.start();
 };
 
-function getParameterByName(name, url='') {
-    if (url.length == 0) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
+function wiggle(aProgress: number, aPeriod1: number, aPeriod2: number): number {
+            var current1: number = aProgress * Math.PI * 2 * aPeriod1;
+            var current2: number = aProgress * (Math.PI * 2 * aPeriod2 + Math.PI / 2);
 
+            return Math.sin(current1) * Math.cos(current2);
+        }
